e9232c702b4e401a30d7ccf94324e3164ad7f084
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-11-28 18:21:27 -0300

    Correçao

--- zeros.py
+++ zeros.py
@@ -11,6 +11,21 @@ def preencher_formulario():
         botao_iniciar.config(bg="SystemButtonFace")
         janela.update()
 
+    data_preenchida = "11/09/2023"
+
+    def descer_seleção():
+        pyautogui.press("down")
+        pyautogui.press("enter")
+        pyautogui.press("tab")
+
+    def preencher_blindagem():
+        #data de blindagem 
+        pyautogui.write(data_preenchida) 
+        pyautogui.press("tab")
+        #meses de blindagem
+        pyautogui.write("22")
+        pyautogui.press("tab")
+
     num_zeros = int(entrada_zeros.get())
 
     # Tempo para clicar o no campo de entrada 
@@ -24,53 +39,63 @@ def preencher_formulario():
         pyautogui.write("0")
         pyautogui.press("tab")
 
-    time.sleep(1)
-    pyautogui.press("down")
-    time.sleep(1)
+    #antecipaçao 
+    pyautogui.write("0.6")
+    pyautogui.press("tab")
+
+    #pula as 9 linhas 
+
+    for _ in range(9):
+        pyautogui.write("0")
+        time.sleep(0.1)
+        pyautogui.press("tab")
+
+    #tem blindagem 
+    descer_seleção()
+    
+    #nao tem tem blindagem 
+    # pyautogui.press("down")
+    # pyautogui.press("down")
+    # pyautogui.press("enter")
+    # pyautogui.press("tab")
+
+    preencher_blindagem()
+    
+    #tem subisidio
+    descer_seleção()
+    #subisio2
+    pyautogui.press("tab")
+    #subisio3
+    pyautogui.press("tab")
+    pyautogui.press("space")
+    pyautogui.press("tab")
+
+    #nao tem subisidio
+    # pyautogui.press("down")
+    # pyautogui.press("down")
+    # pyautogui.press("enter")
+    # #subisio1
+    # pyautogui.press("tab")
+
+    #data de assinatura 
+    pyautogui.write(data_preenchida)
+    pyautogui.press("tab")
     pyautogui.press("enter")
-    time.sleep(1)
+    time.sleep(2)     
     pyautogui.press("tab")
-    time.sleep(1)
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    time.sleep(2)     
+    pyautogui.press("tab")
+    pyautogui.press("tab")
+    time.sleep(1)     
+    pyautogui.write("SODEXO-PC-GSPAR ESTACIONAMENTOS LTDA - EPP-Stewart Motrico Prata-00731633-LAP")
+    time.sleep(1)     
 
-    #inserir a data da blindagem 
-    #pyautogui.write("09/17/2020") 
-    time.sleep(1)
     pyautogui.press("tab")
-    #meses de blindagem 
-    #pyautogui.write("24")
-    pyautogui.press("tab")
-    time.sleep(1)
-    #subisidio se nao  tiver vai ter essa sequencia down, down, enter, tab
-    #pyautogui.press("down")
-    #pyautogui.press("down")
-    #pyautogui.press("enter")
-    #pyautogui.press("tab")
-    #time.sleep(1)
-    #pyautogui.write("09/17/2020")
-    #time.sleep(1)
-    #pyautogui.press("tab")
-    #envio do furmulario 
-    #pyautogui.press("enter")
-    #time.sleep(3)
-    #pyautogui.press("tab")
-    #pyautogui.press("tab")
-    #pyautogui.press("enter")
-    #recaregando para mandar um novo preenchimento
-    #time.sleep(1)
-    #pyautogui.press("tab")
-    #pyautogui.press("tab")
-    #assunto para colocar para fazer o loop
-    #pyautogui.write("ASSUNTOOOOOO")
-    #pyautogui.press("tab")
-    #pyautogui.press("down")
-    #pyautogui.press("enter")
-    #pyautogui.press("tab")
-    #pyautogui.press("down")
-    #pyautogui.press("enter")
-    #passando pelas duas opcoes para selecionar
-    #pyautogui.press("tab")
+    descer_seleção()
+    descer_seleção()
 
-    #restaurando a cor d botao
     restaurar_cor_original()
 
 #interface gráfica
