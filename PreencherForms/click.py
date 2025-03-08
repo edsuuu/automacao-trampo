d62b05852a58ac94536abd8c69cb75070818986a
edsuuu <edsonteles343@gmail.com>
2025-03-08 17:56:52 -0300

    ad

--- PreencherForms/click.py
+++ PreencherForms/click.py
@@ -1,45 +1,33 @@
 import pyautogui
 import time
 
-pyautogui.moveTo(3600, 350) #cnpj
-# time.sleep(0.3)
-# pyautogui.click()
-# pyautogui.hotkey('ctrl', 'c')
-# time.sleep(0.3)
-# pyautogui.moveTo(1000, 840)
-# time.sleep(0.3)
-# pyautogui.click()
-# pyautogui.hotkey('ctrl', 'v')
-# pyautogui.press("tab")
-# time.sleep(0.3)
 
-# pyautogui.moveTo(3600, 320)#razao social
-# pyautogui.click()
-# time.sleep(0.3)
-# pyautogui.hotkey('ctrl', 'c')
-# time.sleep(0.3)
-# pyautogui.moveTo(1000, 585)
-# time.sleep(0.3)
-# pyautogui.click()
-# pyautogui.hotkey('ctrl', 'v')
+def iniciaVarias():
+    pyautogui.moveTo(2150, 905) #INICIA
+    pyautogui.click()
+    pyautogui.moveTo(3350, 605) #INICIA 2 
+    pyautogui.click()
+    time.sleep(2)
+    pyautogui.moveTo(2400, 955)
+    pyautogui.click() # escolhe a rainha
+    pyautogui.moveTo(2250, 565)
+    pyautogui.click() # coloca
 
-# time.sleep(0.3)
-# pyautogui.moveTo(3600, 400)#apagar cnpj
-# time.sleep(0.3)
-# pyautogui.click()
+    pyautogui.moveTo(2050, 815)
+    pyautogui.click() # render
 
-# time.sleep(0.3)
-# pyautogui.keyDown('shift')
-# pyautogui.press('up')
-# pyautogui.press('up')
-# pyautogui.press('up')
-# pyautogui.press('up')
+    pyautogui.moveTo(2980, 615)
+    pyautogui.click()
+    time.sleep(1)
+    pyautogui.moveTo(2880, 915)
+    pyautogui.click()
 
-# pyautogui.keyUp('shift')
-
-# pyautogui.press('backspace')
-
-pyautogui.moveTo(1850, 905)
-pyautogui.click()
-
-# pyautogui.moveTo(1000, 855) 
+def quantidadees():
+    quantide = 20
+    for i in range(quantide): 
+        iniciaVarias()
+        print(i + 1)
+        time.sleep(2)
+        
+quantidadees()
+        
